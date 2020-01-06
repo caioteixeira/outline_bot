77641d5ffc4b40a3c591bfe12fb0208740aa479f
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2020-01-06 20:42:04 -0300

    Remove log

--- test.go
+++ test.go
@@ -13,8 +13,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Print("Hello world sample started.")
-
 	http.HandleFunc("/", handler)
 
 	port := os.Getenv("PORT")
