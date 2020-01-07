7107fdfa1f66111b126be657d7377e4a3aabefdf
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2020-01-07 14:01:48 -0300

    Fix Webhook

--- bot.go
+++ bot.go
@@ -9,18 +9,19 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+/*func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Batata")
 	fmt.Fprint(w, "Bolacha")
-}
+}*/
 
 func main() {
-	http.HandleFunc("/", handler)
 	port := os.Getenv("PORT")
+	/*http.HandleFunc("/", handler)
+
 	if port == "" {
 		port = "8080"
 	}
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))*/
 
 	token := os.Getenv("TELEGRAM_BOT_TOKEN")
 	bot, err := tgbotapi.NewBotAPI(token)
@@ -29,7 +30,7 @@ func main() {
 	}
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(os.Getenv("WEBHOOK_URL") + ":8443/" + bot.Token))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(fmt.Sprintf("%s:%s/%s", os.Getenv("WEBHOOK_URL"), port, bot.Token)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,7 +42,7 @@ func main() {
 		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
 	}
 	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServe("0.0.0.0:8443", nil)
+	go http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil)
 
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
